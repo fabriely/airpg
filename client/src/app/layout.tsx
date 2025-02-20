2f1138d07c348d3f77c166aba4fdd6b41c8d26ed
Fabriely Santos <fabrielysantos15@gmail.com>
2025-02-19 22:38:44 -0300

    adjust layout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,34 +1,24 @@
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
+import NextAuthSessionProvider from "providers/sessionProvider";
 
-import NextAuthSessionProvider from 'providers/sessionProvider';
-import { ThemeProvider } from 'components/theme-provider';
-import 'styles/globals.css';
+import "styles/globals.css";
 
 export const metadata: Metadata = {
-  title: 'Next.js Boilerplate',
-  description: 'A simple boilerplate for next.js',
-  manifest: '/manifest.json'
+  title: "AIRPG",
+  description: "A simple boilerplate for next.js",
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
+    <NextAuthSessionProvider>
     <html>
-      <body>
-        <NextAuthSessionProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-        </NextAuthSessionProvider>
-      </body>
+      <body>{children}</body>
     </html>
+    </NextAuthSessionProvider>
   );
 }
