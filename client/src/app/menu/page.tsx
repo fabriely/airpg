7ab733fe234df191fd42798b3f49701d5148076d
fabriely <flps@cin.ufpe.br>
2025-03-06 17:15:22 -0300

    feat(menu): update layout and improve campaign display with responsive adjustments

--- client/src/app/menu/page.tsx
+++ client/src/app/menu/page.tsx
@@ -52,12 +52,12 @@ export default function Menu() {
     <div className="bg-[#F2F2F2] w-full h-full px-[160px]">
       <Header />
       <div className="mt-[80px] py-[32px]">
-        <h1 className="flex justify-between items-center text-[#191919] font-bold font-grenzen text-[40px]"> 
+        <h1 className="flex justify-between items-center text-[#191919] font-bold font-grenze text-[40px]"> 
           Suas Campanhas
         </h1>
       </div>
 
-      <div className="grid grid-cols-4 grid-flow-row grid-rows-[208px] gap-8">
+      <div className="grid grid-cols-2 grid-flow-row gap-8">
         {campaigns.length > 0 ? (
           campaigns.map((campaign: any) => (
             <Maincard
@@ -70,15 +70,17 @@ export default function Menu() {
                   router.push('/campaign/master');
                 }
               }}
-              className="col-span-1 col-start-1" 
+              className="col-span-1" 
             />
           ))
         ) : (
-          <p>Nenhuma campanha encontrada.</p>
+          <p className="col-span-2">Nenhuma campanha encontrada.</p>
         )}
-        
+      </div>
+
+      <div className="mt-8">
         <Card className="flex justify-center items-center w-full h-full bg-[#E3E3E3] shadow-lg col-span-1 col-start-4 rounded-2xl">
-          <div className="flex flex-col justify-between items-center w-full h-full px-4 py-4 gap-4">
+      <div className="flex flex-col justify-between items-center w-full h-full px-4 py-4 gap-4">
             <button onClick={openModal} className="w-full h-full bg-gradient-to-r from-[#B81414] to-[#8A0F0F] font-crimson font-bold text-[#F2F2F2] text-[20px] rounded-[8px] hover:bg-gradient-to-r hover:from-[#EB4747] hover:to-[#E51919]">
               Entrar Em Uma Campanha
             </button>
@@ -87,15 +89,15 @@ export default function Menu() {
             </button>
           </div>
         </Card>
-        
-        <ModalJoinCampaign
-          isOpen={isOpen}
-          closeModal={closeModal}
-          handleSubmit={handleSubmit}
-          code={code}
-          setCode={setCode}
-        />
       </div>
+        
+      <ModalJoinCampaign
+        isOpen={isOpen}
+        closeModal={closeModal}
+        handleSubmit={handleSubmit}
+        code={code}
+        setCode={setCode}
+      />
     </div>
   );
 }
