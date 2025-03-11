0095fd8f8b3161833ed3a34c5f4c3b0d140c01ef
MusicalUsagi <tavaresbezerra4@gmail.com>
2025-03-11 00:30:11 -0300

    feat(pdf-reader-component): put the dropdown menu on the pdf toolbar

--- client/src/components/campaign/PdfReader.tsx
+++ client/src/components/campaign/PdfReader.tsx
@@ -22,20 +22,16 @@ interface PdfReaderProps extends React.HTMLAttributes<HTMLDivElement> {}
 const PdfReader = React.forwardRef<HTMLDivElement, PdfReaderProps>(
   ({ className, ...props }, ref) => {
     const [selectedPdf, setSelectedPdf] = React.useState<PdfOption>('Player');
-    const defaultLayoutPluginInstance = defaultLayoutPlugin();
     
     const fileUrl = `/pdf/${selectedPdf}.pdf`;
 
-    return (
-      <CampaignPanel 
-        ref={ref}
-        className={cn("flex flex-col items-center p-4 gap-4", className)}
-        {...props}
-      >
-        <div className="w-full flex justify-end">
+    const renderToolbar = (Toolbar: (props: ToolbarProps) => ReactElement) => (
+      <>
+          <Toolbar />
+          <div className="w-full flex">
           <DropdownMenu>
-            <DropdownMenuTrigger className="px-4 py-2 bg-accent text-accent-foreground rounded-md hover:bg-accent/80 transition-colors">
-              Selecionar PDF ({selectedPdf})
+            <DropdownMenuTrigger className="px-2 py-1 bg-accent text-accent-foreground rounded-md hover:bg-accent/80 transition-colors">
+              Selecionar Livro
             </DropdownMenuTrigger>
             <DropdownMenuContent>
               {(['Player', 'Monster', 'Master'] as PdfOption[]).map((option) => (
@@ -49,7 +45,19 @@ const PdfReader = React.forwardRef<HTMLDivElement, PdfReaderProps>(
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
+      </>
+  );
+  
+  const defaultLayoutPluginInstance = defaultLayoutPlugin({
+      renderToolbar,
+  });
 
+    return (
+      <CampaignPanel 
+        ref={ref}
+        className={cn("flex flex-col items-center p-1 gap-16", className)}
+        {...props}
+      >
         <div className="w-full h-[600px] overflow-auto">
           <div className="w-full h-full">
               <Worker workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjsLib.version}/build/pdf.worker.min.js`}>
