73acaee91024eb097f541b1ac0706ad78dcd87dc
Pedro Campelo Lins <pedro.fev03@gmail.com>
2025-03-23 20:51:12 -0300

    Landing Page & Design Overhauls
    feat(visual-overhaul): created a landing page. The login modal is now always appearing on the landing page. "Suas Campanhas" paegs and the gm's "Campaign" page are with a new subtle background.

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -15,7 +15,7 @@ const Header: React.FC = () => {
     };  
 
     return(
-        <header className="fixed top-0 left-0 w-full h-[60px] flex justify-between items-center px-4 md:px-[160px] py-[12px] bg-[#191919] ">
+        <header className="fixed top-0 left-0 w-full h-[64px] flex justify-between items-center px-4 md:px-[160px] py-[12px] bg-[#191919] ">
             <RectangleHorizontal className="text-[#F2F2F2] w-[30px] h-[30px] md:w-[40px] md:h-[40px]" />
 
             <div className="grid grid-cols-2 grid-flow-row gap-2 md:gap-4">
@@ -34,11 +34,6 @@ const Header: React.FC = () => {
                     </>
                 ) : (
                     <>
-                        <button
-                            onClick={openModal}
-                            className="w-[130px] h-[35px] md:w-[150px] md:h-[40px] rounded-[8px] bg-gradient-to-r from-[#F2F2F2] to-[#D4D4D4] text-[#191919] font-crimson font-bold text-[16px] md:text-[20px] hover:bg-gradient-to-r hover:from-[#D4D4D4] hover:to-[#B5B5B5] col-span-1 col-start-1"
-                        > Fazer Login
-                        </button>
                         <button
                             onClick={() => router.push('/signup')}
                             className="w-[130px] h-[35px] md:w-[150px] md:h-[40px] rounded-[8px] bg-gradient-to-r from-[#B81414] to-[#8A0F0F] text-[#F2F2F2] font-crimson font-bold text-[16px] md:text-[20px] hover:bg-gradient-to-r hover:from-[#EB4747] hover:to-[#E51919] col-span-1 col-start-2"
